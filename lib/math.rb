2f1173a6e70e0cdd9a14b8c3379b807fe509aab8
Aude Faucheux <aude53@gmail.com>
2019-07-22 21:01:25 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,15 +4,18 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  calc = num1 - num2
+  return calc
 end
 
 def division(num1, num2)
-
+  calc = num1 / num2
+  return calc
 end
 
 def multiplication(num1, num2)
-
+  calc = num1 * num2
+  return calc
 end
 
 def modulo(num1, num2)
